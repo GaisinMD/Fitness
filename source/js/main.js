4b12044f111cf4e09cf88ad43d02e4a9dff932d3
GaisinMD <goodzilla365@mail.ru>
2019-12-10 21:52:50 +0300

    Акции - desktop

--- source/js/main.js
+++ source/js/main.js
@@ -1,17 +1,6 @@
 'use strict';
-// var mobileMaxWidth = '(max-width: 767px)';
-// var tabletMaxWidth = '(max-width: 1023px)';
-// var itemSlides;
 
-// if (window.matchMedia(mobileMaxWidth).matches) {
-//   itemSlides = 1;
-// } else if (window.matchMedia(tabletMaxWidth).matches) {
-//   itemSlides = 2;
-// } else {
-//   itemSlides = 4;
-// }
-
-var slider = tns({
+var sliderTrainers = tns({
   'container': '.trainers-carousel__list',
   'prevButton': '.trainers-carousel__arrow--left',
   'nextButton': '.trainers-carousel__arrow--right',
