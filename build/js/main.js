c7aa50fbd164b83ecd95e06c8b38afcc81d95c1a
GaisinMD <goodzilla365@mail.ru>
2019-12-11 12:01:29 +0300

    Форма - desktop

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,28 @@
 'use strict';
+// переменные скролла
+var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
+var animationTime = 300;
+var framesCount = 20;
+
+// реализация скролла
+anchors.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+    var scroller = setInterval(function () {
+      var scrollBy = coordY / framesCount;
+
+      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        window.scrollBy(0, scrollBy);
+      } else {
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    }, animationTime / framesCount);
+  });
+});
 
 var sliderTrainers = tns({
   'container': '.trainers-carousel__list',
